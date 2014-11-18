36ce3aa4a73b7e490f9805965481b64c7cecb7c6
unknown <alexeev.sker@gmail.com>
2014-11-18 17:38:18 +0300

    fix bug

--- app/views/include/nav.blade.php
+++ app/views/include/nav.blade.php
@@ -1,3 +1,3 @@
 <li><a href="{{URL::to('ticket')}}">Задачи</a></li>
 <li><a href="{{URL::to('ticket/create')}}">Добавить задачу</a></li>
-<li><a href="#">Баланс</a></li>
+<!--<li><a href="#">Баланс</a></li>-->
