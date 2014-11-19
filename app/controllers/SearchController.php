5c63a27a13a1e4f17229729f04ab357918e935d3
Alexeev Anton <alexeev.sker@gmail.com>
2014-11-20 00:34:07 +0300

    Edit search

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -15,7 +15,10 @@ class SearchController extends Controller{
         $q =Input::get('q',false);
         if($q){
             $qSQL = "%".$q."%";
-            $data = Ticket::join('ticket_comments','tickets.id','=','ticket_comments.ticket_id','left')->whereRaw('title like ? or description like ? or url like ? or ticket_comments.comment like ?',array($qSQL,$qSQL,$qSQL,$qSQL))->select('tickets.id','title','description','url','comment')->paginate(20);
+            $data = Ticket::leftJoin('ticket_comments',function($join) use ($qSQL){
+                $join->on('tickets.id','=','ticket_comments.ticket_id');
+                $join->on('ticket_comments.comment','like',DB::raw('?'));
+            })->whereRaw('title like ? or description like ? or url like ? or comment like ? ',array($qSQL,$qSQL,$qSQL,$qSQL,$qSQL))->select('tickets.id','title','description','url','comment')->paginate(20);
         }
         return View::make('search.index')->with(compact('q','data'));
     }
--- app/views/search/index.blade.php
+++ app/views/search/index.blade.php
@@ -11,16 +11,21 @@
         @foreach($data as $ticket)
             <article>
                 <a href="{{URL::route('ticket.show',array('id'=>$ticket->id))}}">№ {{$ticket->id}}</a> {{preg_replace('/('.$q.')/i','<span style="background:#ACE1AF">\1</span>',$ticket->title)}}
-
                 <?$descF=preg_match('/('.$q.')/i',$ticket->description);?>
                 @if(!empty($descF))
-                    <?$pos = strpos(preg_replace('/('.$q.')/i','<span style="background:#ACE1AF">\1</span>',$ticket->description),'<span');?>
-                    <p>{{substr(preg_replace('/('.$q.')/i','<span style="background:#ACE1AF">\1</span>',$ticket->description),$pos>51?($pos-50):0,35+strlen($q)+35)}}</p>
+                    <?
+                    $pos = strpos(preg_replace('/('.$q.')/i','<span style="background:#ACE1AF">\1</span>',$ticket->description),'<span');
+                    $str =preg_replace('/('.$q.')/i','<span style="background:#ACE1AF">\1</span>',$ticket->description);
+                    $length = ($pos+35+strlen($q)+35)>strlen($str) ? (strlen($str)-$pos):($pos+35+strlen($q)+35);
+                    ?>
+                    <p>{{substr($str,$pos>51?($pos-50):0,$length)}}</p>
                 @endif
                 <?$commentF = preg_match('/('.$q.')/i',$ticket->comment);?>
                 @if(!empty($commentF))
-                    <?$pos = strpos(preg_replace('/('.$q.')/i','<span style="background:#ACE1AF">\1</span>',$ticket->comment),'<span');?>
-                    <p>{{substr(preg_replace('/('.$q.')/i','<span style="background:#ACE1AF">\1</span>',$ticket->comment),$pos>51?($pos-50):0,35+strlen($q)+35)}}</p>
+                    <?$pos = strpos(preg_replace('/('.$q.')/i','<span style="background:#ACE1AF">\1</span>',$ticket->comment),'<span');
+                                          $str =preg_replace('/('.$q.')/i','<span style="background:#ACE1AF">\1</span>',$ticket->comment);
+                                          $length = ($pos+35+strlen($q)+35)>strlen($str) ? (strlen($str)-$pos):($pos+35+strlen($q)+35);?>
+                   <p>{{substr($str,$pos>51?($pos-50):0,$length)}}</p>
                 @endif
             </article>
         @endforeach
