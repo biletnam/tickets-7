c0761002fc8cf28fc08babdd9dcd1414c8b4960e
5programist <5programist@mail.ru>
2014-12-10 14:58:32 +0300

    поменял ссылку на страницу web-kmv.ru

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -13,7 +13,7 @@
     <div class="wrapper bg-style2">
         <header class="header">
             <div class="top-header">
-                <a class="logo" href="/"></a>
+                <a class="logo" href="http://www.web-kmv.ru/"></a>
                 @if (Auth::check())
                     <nav>
                         <ul>
